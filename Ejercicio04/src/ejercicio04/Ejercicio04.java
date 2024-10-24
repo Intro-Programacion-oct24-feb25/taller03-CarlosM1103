b6e732498bbd4e8c1fd86b3ced668a0c4b9cebb4
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-10-24 11:26:01 -0500

    Cuarto ejercicio resuelto

---
+++ Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio04;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor1 = 20;
+        int valor2 = 1;
+        int valor3 = 3;
+        int valor4 = 2;
+        
+        boolean resultado = ((valor1/(valor2 + valor3)) + (valor4) >= (valor3)  ) ;
+        System.out.println(resultado);
+    }
+    
+}
