e2357ada30b1008f350aad472daf47d0d31d0c66
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-10-24 11:36:34 -0500

    Ejercicio 6 resuelto

---
+++ Ejercicio06/src/ejercicio06/Ejercicio06.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio06;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio06 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor1 = 81;
+        int valor2 = 9;
+        int valor3= 3;
+        int valor4 = 10;
+        int valor5= 1;
+        
+        boolean resultado = ((((Math.sqrt(valor1)+(valor2))/(valor3)) == valor2 ) && (valor4 > valor5));
+        System.out.println(resultado);
+    }
+    
+}
