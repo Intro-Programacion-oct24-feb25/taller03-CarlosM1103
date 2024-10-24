229de6a0dfa14c83adc8d39f5cf572e805fbe36b
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-10-24 11:30:19 -0500

    Ejercicio 5 resuelto

---
+++ Ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio05;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor1 = 2;
+        int valor2 = 12;
+        int valor3= 1;
+        int valor4 = 3;
+        
+        double resultado = ((Math.pow(valor1,1)+ (valor2) + (valor3))/(valor4));
+        System.out.println(resultado);
+    }
+    
+}
