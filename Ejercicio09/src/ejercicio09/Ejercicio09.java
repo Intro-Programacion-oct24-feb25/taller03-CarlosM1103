35cc810404bc6f16e5a79fc0a2a6f0a38edb636a
CarlosM1103 <carlosmedinal2006@gmail.com>
2024-10-24 12:13:48 -0500

    Ejercicio 9 resuelto

---
+++ Ejercicio09/src/ejercicio09/Ejercicio09.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio09;
+
+/**
+ *
+ * @author utpl
+ */
+public class Ejercicio09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int valor1 = 81;
+        int valor2 = 25;
+        int valor3 = 10;
+        int valor4 = 50;
+        boolean resultado = (Math.sqrt(valor1)+ Math.sqrt(valor2)* valor3 >= valor4);
+        System.out.println(resultado);
+    }
+    
+}
